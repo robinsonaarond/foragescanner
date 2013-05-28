cb7ff88a6812b1289f58ceccd39c4ca05132ef10
Aaron Robinson <robinsonaarond@gmail.com>
2013-05-28 12:44:23 -0700

    Added new class determiner and test.py show example usage

---
+++ classificator.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+
+# Hold getClass, which polls the database, then parses the description and supplies
+# a classification based on that description.  For unsure guess, will place ? in
+# front of the given class.
+
+def getClass(test):
+	return test
+
---
+++ test.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+from classificator import getClass
+
+myvar = getClass("Hello")
+print myvar
+
+exit()
