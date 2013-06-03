1e8b39484e7ec3ac342da70ef9b3d19786a13b20
Aaron Robinson <robinsonaarond@gmail.com>
2013-06-03 13:17:23 -0700

    Updated test.py to check against all descriptions.

--- test.py
+++ test.py
@@ -1,10 +1,31 @@
 #! /usr/bin/env python
+import sqlite3 as lite 
 
 from classificator import getClass
 myDB = "/Users/aaronr/Documents/fs_raspberrypi_contents/root/home/fs/Documents/fs.db"
 desc = "Del Monte Pineapple Crushed In Its Own Juice"
 
-myvar = getClass(desc,myDB)
-print desc,myvar
+#sqlquery = "select description from food where classification is null"
+sqlquery = "select description from food"
+try:
+	con = None
+	con = lite.connect(myDB)
+	cur = con.cursor() 
+	try:
+		cur.execute(sqlquery)
+		descriptions = cur.fetchall()
+	except Exception, e:
+		print e
+		descriptions = ""
+		pass 
+except Exception, e:
+		print e
+
+desc_list = [x[0] for x in descriptions] 
+
+for desc in desc_list:
+	if desc is not None and desc is not "" and desc is not " ":
+		myvar = getClass(desc,myDB)
+		print "Desc: ",desc,"Class: ",myvar
 
 exit()
